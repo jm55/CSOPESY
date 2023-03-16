28cdfd9c4d1fb32fd701db4bc9844ba3c192dc36
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-03-16 18:18:12 +0800

    Output cleanup on tester

--- MP1/test.py
+++ MP1/test.py
@@ -43,7 +43,7 @@ def loop_files(mode:int, directory:str, files:list):
         else: 
             print("Invalid Mode")
         if output != None:
-            print(output["AveWT"], math.floor(output["AveWT"]), f[1], math.floor(f[1]),  math.floor(output["AveWT"]) == math.floor(f[1]))
+            #print(output["AveWT"], math.floor(output["AveWT"]), f[1], math.floor(f[1]),  math.floor(output["AveWT"]) == math.floor(f[1]))
             if math.floor(output["AveWT"]) == math.floor(f[1]): # and output["AveTT"] == f[2]:
                 out.append(f[0] + " PASS (EWT/AWT:{ewt:.1f}/{awt:.1f})".format(ewt=f[1], ett=f[2], awt=output["AveWT"], tt=output["AveTT"]))
                 passing += 1
